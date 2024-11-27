4ed9fd1f429f94fab7c62981f3a8cfd6f6ebf0ee
emmaworthington <emma.worthington@proton.me>
2024-11-27 12:54:38 +0000

    Restructured directories

--- src/eva_data_analysis.py
+++ src/eva_data_analysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def read_json_to_dataframe(input_file):
     """
     Load data from JSON into a pandas dataframe,
@@ -21,6 +22,7 @@ def read_json_to_dataframe(input_file):
 
     return eva_df
 
+
 def write_dataframe_to_csv(df, output_file):
     """
     Write the pandas dataframe to a CSV file.
@@ -31,27 +33,61 @@ def write_dataframe_to_csv(df, output_file):
     """
 
     # Output data to spreadsheet for further analysis
+    print('Saving to CSV file {}'.format(output_file))
     df.to_csv(output_file, index=False)
 
-# https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = open('eva-data.json', 'r')
-output_file = open('eva-data.csv', 'w')
-graph_file = './cumulative_eva_graph.png'
 
-eva_df = read_json_to_dataframe(input_file)
+def text_to_duration(duration):
+    """
+    Convert text in hours:minutes:seconds to
+    duration in hours.
+    """
 
-# Convert duration to hours instead of hours/minutes/seconds
-eva_df['duration_hours'] = eva_df['duration'].str.split(':').apply(lambda x: int(x[0]) + int(x[1]) / 60)
+    hours, minutes = duration.split(':')
+    duration_hours = int(hours) + int(minutes) / 6
 
-# Calculate cumulative sum of hours
-eva_df['cumulative_hours'] = eva_df['duration_hours'].cumsum()
+    return duration_hours
 
-write_dataframe_to_csv(eva_df, output_file)
 
-# Plot cumulative hours with time
-plt.plot(eva_df['date'], eva_df['cumulative_hours'], 'ko-')
-plt.xlabel('Year')
-plt.ylabel('Total time spent in space to date (hours)')
-plt.tight_layout()
-plt.savefig(graph_file)
-plt.show()
+def add_duration_hours_variable(df):
+
+    df_copy = df.copy()
+    df_copy['duration_hours'] = df_copy['duration'].apply(text_to_duration)
+
+    return df_copy
+
+
+def plot_cumulative_time_in_space(df, graph_file):
+    """
+    Plot cumulative hours in space.
+    """
+
+    print('Plotting cumulative spacewalk duration and saving to {}'.format(graph_file))
+
+    df = add_duration_hours_variable(df)
+
+    # Calculate cumulative sum of hours
+    df['cumulative_hours'] = df['duration_hours'].cumsum()
+
+    plt.figure()
+    plt.plot(df['date'], df['cumulative_hours'], 'ko-')
+    plt.xlabel('Year')
+    plt.ylabel('Total time spent in space to date (hours)')
+    plt.tight_layout()
+    plt.savefig(graph_file)
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    print('-- START --')
+    # https://data.nasa.gov/resource/eva.json (with modifications)
+    input_file = open('eva-data.json', 'r')
+    output_file = open('eva-data.csv', 'w')
+    graph_file = './cumulative_eva_graph.png'
+
+    eva_df = read_json_to_dataframe(input_file)
+
+    write_dataframe_to_csv(eva_df, output_file)
+
+    plot_cumulative_time_in_space(eva_df, graph_file)
